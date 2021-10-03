a98b584158fa799df98d9f0072d2f4b082efcca4
ndyt <ntomlinson@gmail.com>
2021-10-03 16:01:01 -0500

    added quiz_model and quiz_error files

---
+++ quiz_model.py
@@ -0,0 +1,33 @@
+"""
+Model objects 
+"""
+
+class Quizquestion():
+
+    def __init__(self, 
+                id, 
+                question_text, 
+                correct_answer, 
+                wrong_answer_1,
+                wrong_answer_2,
+                wrong_answer_3,
+                topic,
+                difficulty,
+                points):
+
+        self.id = id
+        self.question_text = question_text
+        self.correct_answer = correct_answer
+        self.wrong_answer_1 = wrong_answer_1
+        self.wrong_answer_2 = wrong_answer_2
+        self.wrong_answer_3 = wrong_answer_3
+        self.topic = topic
+        self.difficulty = difficulty
+        self.points = points 
+
+    
+    def __str__(self):
+        return f'{self.id}, {self.question_text}, {self.correct_answer}, {self.topic}, {self.difficulty}, {self.points}'
+
+if __name__ == '__main__':
+    main()
--- starter_file.py
+++ starter_file.py
@@ -7,33 +7,6 @@ from datetime import datetime, date, time
 import uuid
 
 db = 'questions.sqlite'
-
-class Quizquestion:
-
-    def __init__(self, 
-                id, 
-                question_text, 
-                correct_answer, 
-                wrong_answer_1,
-                wrong_answer_2,
-                wrong_answer_3,
-                topic,
-                difficulty,
-                points):
-
-        self.id = id
-        self.question_text = question_text
-        self.correct_answer = correct_answer
-        self.wrong_answer_1 = wrong_answer_1
-        self.wrong_answer_2 = wrong_answer_2
-        self.wrong_answer_3 = wrong_answer_3
-        self.topic = topic
-        self.difficulty = difficulty
-        self.points = points 
-
-    
-    def __str__(self):
-        return f'{self.id}, {self.question_text}: {self.correct_answer}, {self.topic}, {self.difficulty}, {self.points}'
     
 def get_topics():
     conn = sqlite3.connect(db)
